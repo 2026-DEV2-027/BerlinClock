1a04d2b4f7da3aa7c0104e34c7a7dc48e9e2efd0
2026-DEV2-027 <rx7n5y8x45@privaterelay.appleid.com>
2026-01-25 15:02:11 +0100

    chore: create Xcode project

---
+++ BerlinClock/BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-027 on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClock/BerlinClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerlinClock
+//
+//  Created by 2026-DEV2-027 on 25/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
