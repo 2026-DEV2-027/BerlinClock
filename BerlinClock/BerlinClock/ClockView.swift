963f76f98a68e21787516299ddd84d6d5dd77c0f
2026-DEV2-027 <rx7n5y8x45@privaterelay.appleid.com>
2026-01-27 22:54:03 +0100

    chore: remove unused imports

--- BerlinClock/BerlinClock/ClockView.swift
+++ BerlinClock/BerlinClock/ClockView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Combine
 
 struct ClockView: View {
     @StateObject var viewModel: ClockViewModel
--- BerlinClock/BerlinClock/Model/Metronome/CustomMetronome.swift
+++ BerlinClock/BerlinClock/Model/Metronome/CustomMetronome.swift
@@ -5,7 +5,6 @@
 //  Created by 2026-DEV2-027 on 27/01/2026.
 //
 
-import Combine
 import Foundation
 
 class CustomMetronome: MetronomeProtocol {
